b5ad27ed859acb96526a3c4711653f200eec264c
HiFiBerry <support@hifiberry.com>
2026-07-17 10:18:01 +0200

    release: 0.1.2 (version bump above ad-hoc 0.1.1 build)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hifiberry-auth",
-    version="0.1.0",
+    version="0.1.2",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     entry_points={"console_scripts": ["hifiberry-auth=hifiberry_auth.__main__:main"]},
